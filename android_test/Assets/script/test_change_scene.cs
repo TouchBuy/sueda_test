6740cd034e4e14df806a569b96409992ff628f2d
terfno <sueda.takahito@gmail.com>
2018-06-21 18:16:11 +0900

    [add] シーン遷移できた！

---
+++ android_test/Assets/script/test_change_scene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class test_change_scene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Console.WriteLine("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)==true){
+			SceneManager.LoadScene("test");
+		}
+	}
+}
